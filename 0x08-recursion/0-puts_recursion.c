e841d7cd9ae6740ec8ab85d886a564581743087e
Tshedza <tshedzaa@gmail.com>
2024-07-10 13:51:08 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,17 @@
-
 #include "main.h"
+
 /**
- * _puts_recursion - function that prints a string, followed by a new line.
- * @s: input
- * Return: Always 0 (Success)
+ * _puts_recursion - Prints a string
+ * followed by a new line
+ * @s: The string to be printed
  */
 void _puts_recursion(char *s)
 {
-if (*s)
-{
-_putchar(*s);
-_puts_recursion(s + 1);
-}
-
-else
-_putchar('\n');
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
